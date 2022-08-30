4e4404450397b20700fe7b17f265a3bad0f1fc9d
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2022-08-30 10:59:02 -0300

    :lipstick: feat: add fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import {
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_800ExtraBold
+} from '@expo-google-fonts/poppins';
 
 import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          DMSans_400Regular,
+          DMSerifDisplay_400Regular,
+          Poppins_300Light,
+          Poppins_400Regular,
+          Poppins_500Medium,
+          Poppins_700Bold,
+          Poppins_800ExtraBold
+        });
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <StatusBar backgroundColor='transparent' barStyle='light-content' translucent />
       <Home />
     </View>
---
+++ src/styles/index.tsx
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    primary: '#741E29',
+    secondary: '#F9F9F9',
+    success: '#22B07D',
+    alert: '#B83341',
+    text: '#333333',
+    white: '#FFFFFF',
+    background: '#F8F7F7',
+    shape: '#B2B2B2',
+  },
+
+  fonts: {
+    regular: 'DMSans_400Regular',
+    medium: 'DMSans_500Medium',
+    bold: 'DMSans_700Bold',
+  },
+};
